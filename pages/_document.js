63a1e09a9d5c780dbb3d1e2d1894841f506c7d57
communet777 <janabram@posteo.de>
2026-07-28 15:41:21 +0200

    Add gold Flower of Life favicon

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import{Html,Head,Main,NextScript}from'next/document'
+export default function Document(){
+return(
+<Html lang="de">
+<Head>
+<link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+<link rel="shortcut icon" href="/favicon.svg"/>
+<meta name="theme-color" content="#1a1c1a"/>
+</Head>
+<body>
+<Main/>
+<NextScript/>
+</body>
+</Html>
+)
+}
